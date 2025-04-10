66874e0f679c0731a1780ae9454ad1a63b232a23
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-04-10 17:53:23 -0400

    Model class and user pactrices

--- appMethods.py
+++ appMethods.py
@@ -1,1 +1,18 @@
-print("TESTING")
+class Model():
+  table = False
+  def __init__(self):
+    if not self.table:
+      print("Error, Table has not been define ")
+
+  def save (self):
+    print(F"saving {self.table} en BD")
+
+  def  searchById(self,__id):
+      print(f"Search by id : {__id}")
+
+class User(Model):
+  table = "user"
+
+user = User()
+user.save()
+user.searchById("121")
